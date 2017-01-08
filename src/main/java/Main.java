5785f8c6b942e29c80897b2bb8deaf57d98c9d1c
cjw <jamie12221@outlook.com>
2017-01-08 14:16:33 +0800

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Administrator on 2017/1/8 0008.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
